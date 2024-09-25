325ac5000930cd4de2fc234b9e5cbd6a3285f12c
root <root@DESKTOP-UFFG1BQ>
2024-09-25 17:33:32 +0200

    small refactor in poller

--- srcs/Poller.cpp
+++ srcs/Poller.cpp
@@ -7,13 +7,14 @@ Poller::~Poller() {
 }
 
 void	Poller::poll() {
-	struct pollfd arr[this->sockets.size()];
+	size_t size = this->sockets.size();
+	struct pollfd arr[size];
 
-	for (size_t i = 0; i < this->sockets.size(); i++) {
+	for (size_t i = 0; i < size; i++) {
 		arr[i].fd = this->sockets[i].getFd();
 		arr[i].events = POLLHUP | POLLIN | POLLOUT;
 	}
-	::poll(arr, this->sockets.size(), 1000);
+	::poll(arr, size, 1000);
 }
 
 void	Poller::addSocket(int fd) {
