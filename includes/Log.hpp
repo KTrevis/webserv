78772ec53d4265c9bdadeb19a153e86c116bfd85
root <root@DESKTOP-UFFG1BQ>
2024-09-30 10:48:09 +0200

    refactored events debugging, using a map and an iterator instead of an array and a switch, and added a debug level: trace

--- includes/Log.hpp
+++ includes/Log.hpp
@@ -6,15 +6,17 @@ enum e_loglevel {
 	ERROR,
 	INFO,
 	DEBUG,
+	TRACE,
 };
 
 #ifndef LOG_LEVEL
-# define LOG_LEVEL DEBUG
+# define LOG_LEVEL TRACE
 #endif
 
 namespace Log {
 	void		Error(const std::string &str);
 	void		Info(const std::string &str);
 	void		Debug(const std::string &str);
+	void		Trace(const std::string &str);
 	void		Event(int maskEvent);
 };
--- srcs/Log.cpp
+++ srcs/Log.cpp
@@ -1,5 +1,11 @@
 #include "Log.hpp"
 #include <sys/epoll.h>
+#include <map>
+
+void	Log::Error(const std::string &str) {
+	if (LOG_LEVEL >= ERROR)
+		std::cerr << "[ERROR]: " << str << std::endl;
+}
 
 void	Log::Info(const std::string &str) {
 	if (LOG_LEVEL >= INFO)
@@ -11,53 +17,39 @@ void	Log::Debug(const std::string &str) {
 		std::cout << "[DEBUG]: " << str << std::endl;
 }
 
-void	Log::Error(const std::string &str) {
-	if (LOG_LEVEL >= ERROR)
-		std::cerr << "[ERROR]: " << str << std::endl;
+void	Log::Trace(const std::string &str) {
+	if (LOG_LEVEL >= TRACE)
+		std::cout << "[TRACE]: " << str << std::endl;
 }
 
-static std::string eventToStr(int event) {
-	switch (event) {
-		case (EPOLLIN): return "EPOLLIN";
-		case (EPOLLPRI): return "EPOLLPRI";
-		case (EPOLLOUT): return "EPOLLOUT";
-		case (EPOLLRDNORM): return "EPOLLRDNORM";
-		case (EPOLLRDBAND): return "EPOLLRDBAND";
-		case (EPOLLWRNORM): return "EPOLLWRNORM";
-		case (EPOLLWRBAND): return "EPOLLWRBAND";
-		case (EPOLLMSG): return "EPOLLMSG";
-		case (EPOLLERR): return "EPOLLERR";
-		case (EPOLLHUP): return "EPOLLHUP";
-		case (EPOLLRDHUP): return "EPOLLRDHUP";
-		case (EPOLLEXCLUSIVE): return "EPOLLEXCLUSIVE";
-		case (EPOLLWAKEUP): return "EPOLLWAKEUP";
-		case (EPOLLONESHOT): return "EPOLLONESHOT";
-	}
-	return "";
+std::map<EPOLL_EVENTS, std::string> getEventsMap() {
+	std::map<EPOLL_EVENTS, std::string> eventsMap;
+
+	eventsMap[EPOLLIN] = "EPOLLIN";
+	eventsMap[EPOLLPRI] = "EPOLLPRI";
+	eventsMap[EPOLLPRI] = "EPOLLPRI";
+	eventsMap[EPOLLOUT] = "EPOLLOUT";
+	eventsMap[EPOLLRDNORM] = "EPOLLRDNORM";
+	eventsMap[EPOLLRDBAND] = "EPOLLRDBAND";
+	eventsMap[EPOLLWRNORM] = "EPOLLWRNORM";
+	eventsMap[EPOLLWRBAND] = "EPOLLWRBAND";
+	eventsMap[EPOLLMSG] = "EPOLLMSG";
+	eventsMap[EPOLLERR] = "EPOLLERR";
+	eventsMap[EPOLLHUP] = "EPOLLHUP";
+	eventsMap[EPOLLRDHUP] = "EEPOLLRDHUP";
+	eventsMap[EPOLLEXCLUSIVE] = "EPOLLEXCLUSIVE";
+	eventsMap[EPOLLWAKEUP] = "EPOLLWAKEUP";
+	eventsMap[EPOLLONESHOT] = "EPOLLONESHOT";
+	return eventsMap;
 }
 
 void Log::Event(int maskEvent) {
-	const int size = 14;
-	const EPOLL_EVENTS events[size] = {
-		EPOLLIN,
-		EPOLLPRI,
-		EPOLLOUT,
-		EPOLLRDNORM,
-		EPOLLRDBAND,
-		EPOLLWRNORM,
-		EPOLLWRBAND,
-		EPOLLMSG,
-		EPOLLERR,
-		EPOLLHUP,
-		EPOLLRDHUP,
-		EPOLLEXCLUSIVE,
-		EPOLLWAKEUP,
-		EPOLLONESHOT,
-	};
+	std::map<EPOLL_EVENTS, std::string> eventsMap = getEventsMap();
+	std::map<EPOLL_EVENTS, std::string>::iterator it = eventsMap.begin();
 
-	for (int i = 0; i < size; i++) {
-		if (maskEvent & events[i]) {
-			std::string str = "Event happened: " + eventToStr(events[i]);
+	for (; it != eventsMap.end(); it++) {
+		if (maskEvent & it->first) {
+			std::string str = "Event happened: " + it->second;
 			Log::Debug(str);
 		}
 	}
